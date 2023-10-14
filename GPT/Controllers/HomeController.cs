5374206ebfd1fb4094760238b1bb664d194e4a33
Adhil Lawton <50740196+AdhilLawton@users.noreply.github.com>
2023-10-14 21:13:04 +0200

    added models and editors for text areas

--- GPT/Controllers/HomeController.cs
+++ GPT/Controllers/HomeController.cs
@@ -31,6 +31,7 @@ namespace GPT.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        [HttpGet]
         public ActionResult ChatGPT(Question question)
         {
             //Create connection to OpenAi api via secret key
@@ -45,26 +46,31 @@ namespace GPT.Controllers
             //chat.AppendUserInput("Write me a simple program that allows me to add two numbers together in python");
 
 
-            //controller sends text to open ai api
-            string context = question.context = "You are a dog who only barks";
-            string message = question.message = "Say hello to me";
+            if (!ModelState.IsValid)
+            {
+
+                //controller sends text to open ai api
+                string context = question.context = "You are a dog who only barks";
+                string message = question.message = "Say hello to me";
 
 
 
-            //returns text to controller
-            chat.AppendSystemMessage(context);
-            chat.AppendUserInput(message);
+                //returns text to controller
+                chat.AppendSystemMessage(context);
+                chat.AppendUserInput(message);
 
 
 
-            //open ai api returns res
-            string res = chat.GetResponseFromChatbotAsync().Result;
+                //open ai api returns res
+                string res = chat.GetResponseFromChatbotAsync().Result;
 
 
 
-            //string message = "Hello from the controller \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-            
-            return Content(res);
+                //string message = "Hello from the controller \n Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+                return Content(res);
+            }
+            return Content("404");
         }
     }
 }
